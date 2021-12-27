49da61f3fce8971bebc93b3a551f99d5c2c4e9a0
leofmichelon34@gmail.com <leofmichelon34@gmail.com>
2021-12-27 18:55:31 -0300

    correct string when there's only 1 player online
    Thanks @LappySheep for the bug report 👍

--- events/minecraft/chat/getOnline.js
+++ events/minecraft/chat/getOnline.js
@@ -4,8 +4,9 @@ module.exports = {
    name: 'getOnline',
    async execute(numOfOnline) {
       // —— Bot reconnection log
+      plural = numOfOnline - 1 !== 1
       toDiscordChat(
-         `:information_source: Bot has reconnected to Hypixel. There are **${numOfOnline - 1}** other members online.`
+         `:information_source: Bot has reconnected to Hypixel. There ${plural ? 'are' : 'is'} **${numOfOnline - 1}** other member${plural ? 's' : ''} online.`
       );
    },
 };
